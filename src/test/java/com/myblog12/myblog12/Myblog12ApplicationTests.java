cafd4f2163169e5bdeef2a8ab7f74d7eb364cf0d
prachi <prachi@example.com>
2024-01-05 01:05:11 +0530

    127:Create save Post Feature

---
+++ src/test/java/com/myblog12/myblog12/Myblog12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog12.myblog12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
